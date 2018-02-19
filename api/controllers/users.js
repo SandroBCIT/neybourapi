a484d4f57d965307f6c0752adc0b8c1f96f67dea
unknown <alessandro.grunwald@gmail.com>
2018-02-18 23:41:20 -0800

    a

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -6,22 +6,24 @@ const User = require("../models/user");
 
 exports.users_get_all = (req, res, next) => {
     User.find()
-      .select("email password _id")
-      .exec()
-      .then(docs => {
+    .select("_id email password name posts")
+    .exec()
+    .then(docs => {
         const response = {
-          count: docs.length,
-          users: docs.map(doc => {
-            return {
-              _id: doc._id,
-              email: doc.email,
-              password: doc.password,
-              request: {
-                type: "GET",
-                url: "http://localhost:3000/users/" + doc._id
-              }
-            };
-          })
+            count: docs.length,
+            users: docs.map(doc => {
+                return {
+                    _id: doc._id,
+                    email: doc.email,
+                    password: doc.password,
+                    name: doc.name,
+                    posts: doc.posts,
+                    request: {
+                        type: "GET",
+                        url: "http://localhost:3000/users/" + doc._id
+                    }
+                };
+            })
         };
         //   if (docs.length >= 0) {
         res.status(200).json(response);
@@ -30,111 +32,100 @@ exports.users_get_all = (req, res, next) => {
         //           message: 'No entries found'
         //       });
         //   }
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json({
-          error: err
-        });
-      });
-  }
+    })
+    .catch(err => {
+        res.status(500).json({ error: err });
+    });
+}
 
-  exports.users_signup_user = (req, res, next) => {
+exports.users_signup_user = (req, res, next) => {
     User.find({ email: req.body.email })
-      .exec()
-      .then(user => {
+    .exec()
+    .then(user => {
+        //checks if email already exists
         if (user.length >= 1) {
-          return res.status(409).json({
-            message: "Mail exists"
-          });
+            return res.status(409).json({
+                message: "Email already exists"
+            });
         } else {
-          bcrypt.hash(req.body.password, 10, (err, hash) => {
-            if (err) {
-              return res.status(500).json({
-                error: err
-              });
-            } else {
-              const user = new User({
-                _id: new mongoose.Types.ObjectId(),
-                email: req.body.email,
-                password: hash
-              });
-              user
-                .save()
-                .then(result => {
-                  console.log(result);
-                  res.status(201).json({
-                    message: "User created"
-                  });
-                })
-                .catch(err => {
-                  console.log(err);
-                  res.status(500).json({
-                    error: err
-                  });
-                });
-            }
-          });
+            // encrypts PW
+            bcrypt.hash(req.body.password, 10, (err, hash) => {
+                if (err) {
+                    return res.status(500).json({ error: err });
+                } else {
+                    // creates new user 
+                    const user = new User({
+                        _id: new mongoose.Types.ObjectId(),
+                        email: req.body.email,
+                        password: hash,
+                        name: req.body.name
+                    });
+                    user
+                    .save()
+                    .then(result => {
+                        res.status(201).json({
+                            message: "User created"
+                        });
+                    })
+                    .catch(err => {
+                        res.status(500).json({ error: err });
+                    });
+                }
+            });
         }
-      });
-  }
+    });
+}
 
-  exports.users_login_user = (req, res, next) => {
+exports.users_login_user = (req, res, next) => {
     User.find({ email: req.body.email })
-      .exec()
-      .then(user => {
+    .exec()
+    .then(user => {
         if (user.length < 1) {
-          return res.status(401).json({
-            message: "Auth failed"
-          });
+            return res.status(401).json({
+                message: "Auth failed"
+            });
         }
         bcrypt.compare(req.body.password, user[0].password, (err, result) => {
-          if (err) {
-            return res.status(401).json({
-              message: "Auth failed"
+            if (err) {
+                return res.status(401).json({
+                    message: "Auth failed"
+                });
+            }
+            if (result) {
+                const token = jwt.sign(
+                    {
+                        email: user[0].email,
+                        userId: user[0]._id
+                    },
+                        process.env.JWT_KEY,
+                    {
+                        expiresIn: "1h"
+                    }
+                );
+                return res.status(200).json({
+                    message: "Auth successful",
+                    token: token
+                });
+            }
+            res.status(401).json({
+                message: "Auth failed"
             });
-          }
-          if (result) {
-            const token = jwt.sign(
-              {
-                email: user[0].email,
-                userId: user[0]._id
-              },
-              process.env.JWT_KEY,
-              {
-                  expiresIn: "1h"
-              }
-            );
-            return res.status(200).json({
-              message: "Auth successful",
-              token: token
-            });
-          }
-          res.status(401).json({
-            message: "Auth failed"
-          });
         });
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json({
-          error: err
-        });
-      });
-  }
+    })
+    .catch(err => {
+        res.status(500).json({ error: err });
+    });
+}
 
-  exports.users_delete_user = (req, res, next) => {
+exports.users_delete_user = (req, res, next) => {
     User.remove({ _id: req.params.userId })
-      .exec()
-      .then(result => {
+    .exec()
+    .then(result => {
         res.status(200).json({
-          message: "User deleted"
+            message: "User deleted"
         });
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json({
-          error: err
-        });
-      });
-  }
+    })
+    .catch(err => {
+        res.status(500).json({ error: err });
+    });
+}
