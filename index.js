58995b80f492bea3e1b5c029ce493969a0c92783
unknown <alessandro.grunwald@gmail.com>
2018-02-26 11:58:35 -0800

    reverted merge

--- index.js
+++ index.js
@@ -1,68 +1,8 @@
-const server = require('http').Server();
+const http = require('http');
+const app = require('./app');
 
 const port = process.env.PORT || 4567;
 
-// const server = http.createServer();
+const server = http.createServer(app);
 
 server.listen(port);
-
-
-
-
-const express = require("express");
-const app = express();
-const morgan = require("morgan");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-
-const postRoutes = require("./api/routes/posts");
-// const orderRoutes = require("./api/routes/orders");
-const userRoutes = require('./api/routes/users');
-
-mongoose.connect(
-    "mongodb://admin:" + process.env.MONGO_ATLAS_PW + "@neybour-shard-00-00-hd6e2.mongodb.net:27017,neybour-shard-00-01-hd6e2.mongodb.net:27017,neybour-shard-00-02-hd6e2.mongodb.net:27017/test?ssl=true&replicaSet=neybour-shard-0&authSource=admin",
-    {
-        useMongoClient: true
-    }
-);
-mongoose.Promise = global.Promise;
-
-app.use(morgan("dev"));
-app.use('/uploads', express.static('uploads'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-    return res.status(200).json({});
-  }
-  next();
-});
-
-// Routes which should handle requests
-app.use("/posts", postRoutes);
-// app.use("/orders", orderRoutes);
-app.use("/users", userRoutes);
-
-app.use((req, res, next) => {
-  const error = new Error("Not found");
-  error.status = 404;
-  next(error);
-});
-
-app.use((error, req, res, next) => {
-  res.status(error.status || 500);
-  res.json({
-    error: {
-      message: error.message
-    }
-  });
-});
-
-module.exports = app;
