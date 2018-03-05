c1ebd59ff0c867193378350306e597072e197638
alessandro.grunwald@gmail.com <alessandro.grunwald@gmail.com>
2018-03-04 16:35:56 -0800

    added hoods

--- api/routes/hoods.js
+++ api/routes/hoods.js
@@ -6,7 +6,4 @@ const hoodsController = require('../controllers/hoods');
 // gets regions
 router.get("/", hoodsController.hoods_get_all);
 
-// add regions obj
-router.post("/", hoodsController.hoods_create_hoods);
-
 module.exports = router;
